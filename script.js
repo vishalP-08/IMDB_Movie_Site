37c2b86ad89af4161d097ccbc27cbd5eb7a91bef
Vishal <vishalpandey1718@gmail.com>
2023-09-16 15:40:47 +0530

    Some Touch-Ups

--- script.js
+++ script.js
@@ -4,46 +4,64 @@ async function searchMovies() {
   const moviesContainer = document.getElementById('moviesContainer');
   const loader = document.getElementById('loader');
 
-  // Clear previous results
-  moviesContainer.innerHTML = '';
-  
   // Display loader
   loader.classList.remove('hidden');
-  
+  // Hide previous results
+  moviesContainer.innerHTML = '';  // Clear previous results
+
   const apiUrl = `https://www.omdbapi.com/?s=${encodeURIComponent(movieTitle)}&apikey=${apiKey}`;
+  
   
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    // Hide loader
+   // Hide loader after 3 seconds
+   setTimeout(() => {
     loader.classList.add('hidden');
+    // Show the results container
+    moviesContainer.classList.remove('hidden');
+  }, 3000); // Display spinner for 3 seconds
 
-    if (data.Search) {
-      data.Search.forEach(movie => {
-        const movieDiv = document.createElement('div');
-        movieDiv.classList.add('movie');
+  if (data.Search) {
+    data.Search.forEach(movie => {
+      // Display movie card
+      const movieDiv = document.createElement('div');
+      movieDiv.classList.add('movie');
 
-        const movieTitle = document.createElement('h3');
-        movieTitle.innerText = movie.Title;
+      // Movie title
+      const movieTitleElement = document.createElement('h3');
+      movieTitleElement.innerText = `Title: ${movie.Title}`;
 
-        const movieYear = document.createElement('p');
-        movieYear.innerText = `Year: ${movie.Year}`;
+      // Release year
+      const yearElement = document.createElement('p');
+      yearElement.innerText = `Year: ${movie.Year}`;
 
-        const movieType = document.createElement('p');
-        movieType.innerText = `Type: ${movie.Type}`;
+      // Movie type
+      const typeElement = document.createElement('p');
+      typeElement.innerText = `Type: ${movie.Type}`;
 
-        const moviePoster = document.createElement('img');
-        moviePoster.src = movie.Poster;
-        moviePoster.alt = `${movie.Title} Poster`;
+      // IMDb link as a button
+      const imdbButton = document.createElement('a');
+      imdbButton.href = `https://www.imdb.com/title/${movie.imdbID}/`;
+      imdbButton.innerText = 'IMDb';
+      imdbButton.classList.add('imdb-button');
+      imdbButton.target = '_blank';
 
-        movieDiv.appendChild(movieTitle);
-        movieDiv.appendChild(movieYear);
-        movieDiv.appendChild(movieType);
-        movieDiv.appendChild(moviePoster);
+      // Movie poster
+      const posterUrl = movie.Poster === 'N/A' ? 'placeholder.png' : movie.Poster;
+      const posterImage = document.createElement('img');
+      posterImage.src = posterUrl;
+      posterImage.alt = `${movie.Title} Poster`;
 
-        moviesContainer.appendChild(movieDiv);
-      });
+      movieDiv.appendChild(movieTitleElement);
+      movieDiv.appendChild(yearElement);
+      movieDiv.appendChild(typeElement);
+      movieDiv.appendChild(imdbButton);
+      movieDiv.appendChild(posterImage);
+
+      moviesContainer.appendChild(movieDiv);
+    });
     } else {
       alert('No movies found.');
     }
